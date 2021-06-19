bf22cf6c4d2a305ebb9d9015b6314af9f7375c8b
Oskar <38985619+pinholeye@users.noreply.github.com>
2021-06-19 16:48:11 +0200

    Update max_seq_value.asm

--- max_seq_value.asm
+++ max_seq_value.asm
@@ -13,7 +13,7 @@ main:		la $t0 array
 		la $t1 end
 		lw $t3 ($t0)           
 
-loop:		bgt $t0 $t1 endloop 	# if t0 > t1 --> endlopp
+loop:		bgt $t0 $t1 endloop 		# if t0 > t1 --> endlopp
 		addi $t0 $t0 4          	# next value load into t0
 		lw $t2 ($t0)
 		blt $t2 $t3 loop		# if t2 < t3 --> loop
